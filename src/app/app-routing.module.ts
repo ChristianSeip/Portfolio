b93dbb9b3b46c2735b2927190c69fda12fd707b9
Christian Seip <christian.seip@ravitar.com>
2022-05-25 20:10:01 +0200

    Page Frame and Navigation added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {AboutComponent} from "./pages/about/about.component";
+import {PortfolioComponent} from "./pages/portfolio/portfolio.component";
+import {ContactComponent} from "./pages/contact/contact.component";
+import {LegalNoticeComponent} from "./pages/legal-notice/legal-notice.component";
+import {PrivacyComponent} from "./pages/privacy/privacy.component";
+import {NotFoundComponent} from "./pages/not-found/not-found.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', redirectTo: '', pathMatch: 'full'},
+  {path: 'about', component: AboutComponent},
+  {path: 'portfolio', component: PortfolioComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'legal-notice', component: LegalNoticeComponent},
+  {path: 'legal', redirectTo: 'legal-notice', pathMatch: 'full'},
+  {path: 'privacy', component: PrivacyComponent},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { LegalNoticeComponent } from './pages/legal-notice/legal-notice.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ContactComponent,
+    LegalNoticeComponent,
+    PrivacyComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
