6bb890698fcd48e08503bc6da04e79422e54fbbd
Christian Seip <christian.seip@ravitar.com>
2022-05-26 12:36:35 +0200

    Responsive Design and Navbar Animation added.

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,17 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import {Router} from "@angular/router";
+import {trigger, state, style, animate, transition, keyframes} from '@angular/animations';
+import {BreakpointObserver, BreakpointState} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state('open', style({
+      })),
+      state('closed', style({
+        display: 'none'
+      })),
+      transition('open => closed', [
+        animate("1s", keyframes([
+          style({ opacity: 1, transform: 'translateX(0)'}),
+          style({ opacity: 0, transform: 'translateX(-250px)'}),
+        ]))
+      ]),
+      transition('closed => open', [
+        animate("1s", keyframes([
+          style({ display: 'flex', opacity: 0, transform: 'translateX(-250px)'}),
+          style({ opacity: 1, transform: 'translateX(0)'}),
+        ]))
+      ]),
+    ]),
+  ],
 })
 export class SidebarComponent implements OnInit {
 
-  constructor(public router: Router) {
+  isOpen: boolean = true;
+
+  constructor(public router: Router, private breakpointObserver: BreakpointObserver,) {
+    this.breakpointObserver.observe(["(max-width: 768px)"]).subscribe((result: BreakpointState) => {
+      if(result.matches && this.isOpen) {
+        this.toggle();
+      }
+    });
   }
 
   ngOnInit(): void {
+  }
+
+  toggle() {
+    this.isOpen = !this.isOpen;
   }
 
 }
